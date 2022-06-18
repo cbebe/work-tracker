4c0e4c219712fd14949daa65cade0cf38a3a50ab
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2022-06-17 20:49:00 -0600

    Completely cover CLI

--- cli_test.go
+++ cli_test.go
@@ -10,10 +10,14 @@ import (
 
 type WorkServiceSpy struct {
 	*WorkService
-	CalledStartWork bool
-	CalledStopWork  bool
-	Type            string
-	ID              string
+	CalledStartWork   bool
+	CalledStopWork    bool
+	CalledStartLog    string
+	CalledStopLog     string
+	CalledGetWork     bool
+	CalledGetWorkType string
+	Type              string
+	ID                string
 }
 
 func (w *WorkServiceSpy) StartWork() error {
@@ -26,38 +30,87 @@ func (w *WorkServiceSpy) StopWork() error {
 	return nil
 }
 
-func TestCLI(t *testing.T) {
+func (w *WorkServiceSpy) StartLog(t, u string) error {
+	w.CalledStartLog = t
+	return nil
+}
+
+func (w *WorkServiceSpy) StopLog(t, u string) error {
+	w.CalledStopLog = t
+	return nil
+}
+
+func (w *WorkServiceSpy) GetWork(u string) ([]Work, error) {
+	w.CalledGetWork = true
+	return []Work{}, nil
+}
+
+func (w *WorkServiceSpy) GetWorkType(t, u string) ([]Work, error) {
+	w.CalledGetWorkType = t
+	return []Work{}, nil
+}
+
+func TestHandleCommand_Usage(t *testing.T) {
 	t.Run("it prints the usage when no arguments are given", func(t *testing.T) {
-		spy := StoreSpy{}
-		s := NewWorkService(&spy)
-		stdout := &bytes.Buffer{}
-		args := []string{"test"}
-		err := HandleCommand(stdout, args, s)
+		_, err := executeCommand(t)
 		assert.Error(t, err)
 	})
 
 	t.Run("it prints the usage when an unknown argument is given", func(t *testing.T) {
-		spy := StoreSpy{}
-		s := NewWorkService(&spy)
-		stdout := &bytes.Buffer{}
-		args := []string{"test", "unknown"}
-		err := HandleCommand(stdout, args, s)
+		_, err := executeCommand(t, "unknown")
 		assert.Error(t, err)
 	})
+}
 
+func TestHandleCommand_Start(t *testing.T) {
 	t.Run("it calls StartWork on `start`", func(t *testing.T) {
-		s := &WorkServiceSpy{}
-		stdout := &bytes.Buffer{}
-		args := []string{"test", "start"}
-		HandleCommand(stdout, args, s)
+		s, _ := executeCommand(t, "start")
 		assert.True(t, s.CalledStartWork)
 	})
 
-	t.Run("it calls StopWork on `start`", func(t *testing.T) {
-		s := &WorkServiceSpy{}
-		stdout := &bytes.Buffer{}
-		args := []string{"test", "stop"}
-		HandleCommand(stdout, args, s)
+	t.Run("it calls StartLog on `start <type>`", func(t *testing.T) {
+		wt := "test"
+		s, _ := executeCommand(t, "start", wt)
+		assert.Equal(t, wt, s.CalledStartLog)
+	})
+}
+
+func TestHandleCommand_Stop(t *testing.T) {
+	t.Run("it calls StopWork on `stop`", func(t *testing.T) {
+		s, _ := executeCommand(t, "stop")
 		assert.True(t, s.CalledStopWork)
 	})
+
+	t.Run("it calls StopWork on `stop <type>`", func(t *testing.T) {
+		wt := "test"
+		s, _ := executeCommand(t, "stop", wt)
+		assert.Equal(t, wt, s.CalledStopLog)
+	})
+}
+
+func TestHandleCommand_Get(t *testing.T) {
+	t.Run("it calls GetWork on `get`", func(t *testing.T) {
+		s, _ := executeCommand(t, "get")
+		assert.True(t, s.CalledGetWork)
+	})
+
+	t.Run("it calls GetWorkType on `get <type>`", func(t *testing.T) {
+		wt := "test"
+		s, _ := executeCommand(t, "get", wt)
+		assert.Equal(t, wt, s.CalledGetWorkType)
+	})
+
+	t.Run("it falls through on `list`", func(t *testing.T) {
+		s, _ := executeCommand(t, "list")
+		assert.True(t, s.CalledGetWork)
+	})
+}
+
+func executeCommand(t testing.TB, args ...string) (*WorkServiceSpy, error) {
+	t.Helper()
+	s := &WorkServiceSpy{}
+	stdout := &bytes.Buffer{}
+	oa := append([]string{"cli"}, args...)
+	err := HandleCommand(stdout, oa, s)
+	return s, err
 }
